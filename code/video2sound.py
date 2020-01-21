69a589099863c2d769f3049cbcc5486e1bb99e6a
Davide Sforza <sforza.1710262@studenti.uniroma1.it>
2020-01-21 19:39:29 +0100

    Create directory e main

---
+++ code/video2sound.py
@@ -0,0 +1,12 @@
+from argparse import ArgumentParser
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument("input_video", help="The path of the input video")
+    parser.add_argument("-o", "--output", help="The path of the output file", default="RecoveredSound.wav")
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_args()
