b4663314286dfa7a7944c578c402e0accf291a45
Davide Sforza <sforza.1710262@studenti.uniroma1.it>
2020-02-13 13:11:19 +0100

    Bugfix

--- video2sound.py
+++ video2sound.py
@@ -8,7 +8,7 @@ def parse_args():
   parser = ArgumentParser()
   parser.add_argument('input_video', help='The path of the input video')
   parser.add_argument('-o', '--output', help='The path of the output file', default='RecoveredSound.wav')
-  parser.add_argument('-s', '--sampling-rate', help='The video sampling rate', type='int', default=None)
+  parser.add_argument('-s', '--sampling-rate', help='The video sampling rate', type=int, default=None)
 
   return parser.parse_args()
 
@@ -17,7 +17,7 @@ if __name__ == '__main__':
   args = parse_args()
 
   vr = cv.VideoCapture(args.input_video)
-  sr = v_hsandle.get(cv.CAP_PROP_FPS) if args.sampling_rate is None else args.sampling_rate
+  sr = vr.get(cv.CAP_PROP_FPS) if args.sampling_rate is None else args.sampling_rate
 
   x, _ = sound_from_video(vr, 1, 2, downsample_factor=0.1, sampling_rate=sr)
 
